044eb839d33f8f93b5ca910b62ccd39e2ffc4673
Harry Biddle <harrybiddle@users.noreply.github.com>
2023-10-15 20:23:58 +0200

    Nice

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,18 +3,32 @@
 </header>
 
 <section>
+	<a href="/blog/loan-interest-formulae">
+		<hgroup>
+			<h8 style="color: #fdd835">October 2023</h8>
+			<h4 style="color: var(--h4-color); font-size: var(--font-size)">
+				Formulae for the interest on a loan
+			</h4>
+		</hgroup>
+		<p>
+			I was recently doing some spreadsheets to work out the impact of taking
+			on various loans. Sure, I could have searched for the solutions online,
+			but it was a fun exercise to come up with the equations myself.
+		</p>
+	</a>
 	<hr />
 	<a href="/blog/renting-versus-buying">
 		<hgroup>
 			<h8 style="color: #fdd835">November 2022</h8>
-			<h4 style="color: var(--h4-color); font-size: var(--font-size)">Renting Versus Buying</h4>
+			<h4 style="color: var(--h4-color); font-size: var(--font-size)">
+				Renting versus buying
+			</h4>
 		</hgroup>
-	<p>
-		Should I buy, or should I rent? On the face of it it seems like a house would be
-		a  sort of savings account. All that money you would have otherwise lost to rent!
-		Yet  there are also big financial downsides to buying property [...]
-	</p>
-		</a>
-
+		<p>
+			Should I buy, or should I rent? On the face of it it seems like a house
+			would be a sort of savings account. All that money you would have otherwise
+			lost to rent! Yet there are also big financial downsides to buying property.
+		</p>
+	</a>
 	<hr />
 </section>
