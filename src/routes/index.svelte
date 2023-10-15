41e6025892456da32008e879633d0b49c3a3ed15
Harry Biddle <harrybiddle@users.noreply.github.com>
2023-10-15 20:25:25 +0200

    New post

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,19 +3,31 @@
 </header>
 
 <section>
+	<a href="/blog/loan-interest-formulae">
+		<hgroup>
+			<h8 style="color: #fdd835">October 2023</h8>
+			<h4 style="color: var(--h4-color); font-size: var(--font-size)">
+				Formulae for the interest on a loan
+			</h4>
+		</hgroup>
+		<p>
+			Sure, I could have searched for the solutions online, but it was a fun
+			exercise to come up with the equations myself.
+		</p>
+	</a>
 	<hr />
 	<a href="/blog/renting-versus-buying">
 		<hgroup>
 			<h8 style="color: #fdd835">November 2022</h8>
-			<h4 style="color: var(--h4-color); font-size: var(--font-size)">Renting Versus Buying</h4>
+			<h4 style="color: var(--h4-color); font-size: var(--font-size)">
+				Renting versus buying
+			</h4>
 		</hgroup>
+		<p>
+			To buy or to rent? On the face of it it seems straightforward. In reality
+			there are also big financial downsides to buying property that might not be
+			immediately obvious.
+		</p>
 	</a>
-	<p>
-		To buy or to rent? On the face of it it seems straightforward. If you rent, you fritter away
-		eye-watering monthly sums to your landlord. If you buy, on the other hand, that same hard-earned
-		money flows into your very own house-shaped savings account for you to draw upon in your old
-		age. [...]
-	</p>
-
 	<hr />
 </section>
