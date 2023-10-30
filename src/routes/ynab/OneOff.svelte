79800b00abac1bdc7690500e5ea07c93e594963e
Harry Biddle <harrybiddle@users.noreply.github.com>
2023-10-30 10:54:35 +0100

    Add one-off table

--- src/routes/ynab/OneOff.svelte
+++ src/routes/ynab/OneOff.svelte
@@ -1,43 +1,33 @@
+<svelte:head>
+    <link
+        rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/@picocss/pico@next/css/pico.colors.min.css"
+    />
+</svelte:head>
+
 <script>
     import * as d3 from 'd3';
 
     import { parseMonth } from "./ynab";
 
     export let budgets;
-    const parsedCategories = budgets.map(budget => budget.month.categories
-        .map(parseMonth)
-    )
-
-    const filteredBudgets = budgets.map((budget_) => {
-      // exclude some categories
-      return budget_
-        .map((category) => ({
-          // move "uncategorized" into the "regular" group
-          ...category,
-          group: category.category == "Uncategorized" ? "Regular" : category.group
+    const parsedCategories = budgets.map(budget => {
+        const monthString = budget.month.month;  // e.g. "2023-10-01"
+        return budget.month.categories.map(d => ({
+            ...parseMonth(d),
+            month: monthString
         }))
-        .filter(
-          (category) =>
-            !(
-              //category.obj.hidden |
-              (
-                category.obj.deleted |
-                (category.group == "Internal Master Category") |
-                (category.category == "Forex") |
-                (category.category == "Off The Books")
-              )
-            )
-        );
     })
 
-    const oneOffData = filteredBudgets
-      .map((budget) =>
+    const rows = parsedCategories
+      .map(budget =>
         budget
           // only look at categories in the "One-Off" group
           .filter((e) => e.group === "One-Off")
           // sum up all activity and budget for this group
           .reduce(
             (accumulator, e) => ({
+              month: e.month,
               activity: accumulator.activity + e.activity,
               budgeted: accumulator.budgeted + e.budgeted
             }),
@@ -63,4 +53,40 @@
       .slice()
       .reverse()
 
+    const format = x => d3.format(",.2r")(Math.round(x / 10) * 10)
+    const formatZero = x => x == 0 ? "-" : format(x);
+    const colour = x => x < 0 ? "var(--pico-color-green-350)" : "var(--pico-color-orange-400)";
+    const getMonthFromString = monthString => {
+          const months = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+          ];
+          const date = new Date(monthString);
+          const monthIndex = date.getMonth();
+          return months[monthIndex];
+    }
+
 </script>
+
+<style>
+    table td:nth-child(n+2), table th:nth-child(n+2)  {
+        text-align: right;
+    }
+</style>
+
+<table>
+    <tr>
+        <th>Month</th>
+        <th>Budgeted</th>
+        <th>Excess</th>
+        <th>Cumulative</th>
+    </tr>
+    {#each rows as row}
+        <tr>
+            <td>{getMonthFromString(row.month)}</td>
+            <td>{formatZero(row.budgeted)}</td>
+            <td style="color: {colour(row.newDebt)}">{formatZero(row.newDebt)}</td>
+            <td style="color: {colour(row.cumulativeDebt)}">{formatZero(row.cumulativeDebt)}</td>
+        </tr>
+    {/each}
+</table>
